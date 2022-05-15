a733b75c8acfdb788112b28c84b82645fa64e1fb
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2022-05-15 14:46:50 +0300

    add phone to contact + restyle

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,7 +1,7 @@
 <div class="bg-slate-800 p-2 text-white">
 	<h1 class="mt-10 text-center text-4xl font-semibold">Don't hesitate to contact us !</h1>
 
-	<form class="mx-auto mt-10 max-w-3xl">
+	<form class="mx-auto mt-10 max-w-3xl font-light">
 		<div class="mt-8">
 			<label class="my-2 block text-xl font-thin" for="name">Name:</label>
 			<input
@@ -10,6 +10,7 @@
 				id="name"
 			/>
 		</div>
+
 		<div class="mt-8">
 			<label class="my-2 block text-xl font-thin" for="email">Email:</label>
 			<input
@@ -18,6 +19,7 @@
 				id="email"
 			/>
 		</div>
+
 		<div class="mt-8">
 			<label class="my-2 block text-xl font-thin" for="subject">Subject:</label>
 			<input
@@ -26,6 +28,19 @@
 				id="subject"
 			/>
 		</div>
+
+		<div class="mt-8">
+			<label class="my-2 block text-xl font-thin" for="phone">Phone:</label>
+			<div class="flex items-center">
+				<div disabled class="rounded-l bg-gray-200 py-2 px-3 text-xl text-slate-900">++965</div>
+				<input
+					class="block w-full rounded-r py-2 px-3 text-xl text-slate-900"
+					type="number"
+					id="phone"
+				/>
+			</div>
+		</div>
+
 		<div class="mt-8">
 			<label class="my-2 block text-xl font-thin" for="message">Message:</label>
 			<textarea
@@ -37,7 +52,7 @@
 		</div>
 
 		<button
-			class="m-5 mt-10 mx-auto block rounded-xl bg-lime-300 py-3 px-5 text-2xl font-extralight text-gray-800"
+			class="m-5 mx-auto mt-10 block rounded-xl bg-lime-300 py-3 px-5 text-2xl font-extralight text-gray-800"
 			>Submit</button
 		>
 	</form>
