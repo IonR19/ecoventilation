d283da86806a2caae12184e0e1c9fce489d6b8d0
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2022-05-16 06:47:23 +0300

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,9 @@
+const x = require('tailwindcss/tailwind-config')
+
+/**
+ * @property  {require('tailwindcss/tailwind-config')};
+ */
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
