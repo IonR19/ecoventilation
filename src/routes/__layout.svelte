066764632f38d4be25061e16fdbfda7ab5a80b4a
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2022-06-01 20:33:18 +0300

    add and configure nprogress

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,24 @@
 	import '../app.css';
 	import Footer from '../lib/components/Footer.svelte';
 	import Navbar from '../lib/components/Navbar.svelte';
+
+	import nProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	import 'nprogress/nprogress.css';
+
+	nProgress.configure({
+		minimum: 0.16,
+	});
+
+	$: {
+		if ($navigating) {
+			nProgress.start();
+		}
+		if (!$navigating) {
+			nProgress.done();
+		}
+	}
 </script>
 
 <Navbar />
